2436cea094c16469dfb6e65bd8f8da234b2fd78f
Jordi <jordilop@gmail.com>
2022-12-17 10:33:30 +0100

    Exercici 5

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
+import "./styles.css";
+
 function Home() {
     return (
         <div>
-            <p>Home Content</p>
+            <h3>Welcome to Sprint 8 | Star Wars</h3>
+            <p>This is the Star Wars project for IT Academy, using swapi Star Wars API to show some information about starships, films, pilots and more.</p>
+            <p>More information about swapi Star Wars API, <a href="https://swapi.dev/">here.</a></p>
         </div>
     );
 }
